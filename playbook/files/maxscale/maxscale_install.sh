acd1b3a2ba170ef04da3836ccd06bb42f6c226dd
Emerson Gaudencio <emerson.fender@gmail.com>
2020-05-05 02:02:50 +0100

    Update fix

--- playbook/files/maxscale/maxscale_install.sh
+++ playbook/files/maxscale/maxscale_install.sh
@@ -7,6 +7,31 @@ then
 echo "$verify_maxscale is installed!"
 else
    ### PG Repo #####
+   ##### FIREWALLD DISABLE #########################
+   systemctl disable firewalld
+   systemctl stop firewalld
+   ######### SELINUX ###############################
+   sed -ie 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+   # disable selinux on the fly
+   /usr/sbin/setenforce 0
+
+   ### clean yum cache ###
+   rm -rf /etc/yum.repos.d/MariaDB.repo
+   rm -rf /etc/yum.repos.d/mariadb.repo
+   rm -rf /etc/yum.repos.d/mysql-community.repo
+   rm -rf /etc/yum.repos.d/mysql-community-source.repo
+   rm -rf /etc/yum.repos.d/percona-original-release.repo
+   yum clean headers
+   yum clean packages
+   yum clean metadatas
+
+   ####### PACKAGES ###########################
+   # -------------- For RHEL/CentOS 7 --------------
+   yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+   ### install pre-packages ####
+   yum -y install screen nload bmon openssl libaio rsync snappy net-tools wget nmap htop dstat sysstat
+
    curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
    yum -y install maxscale
 
